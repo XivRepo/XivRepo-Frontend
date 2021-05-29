204b18dd137fb5a03f7af14d7312f3cb4b844448
NightYagi <koruten@gmail.com>
2021-05-29 14:06:44 -0700

    Use context instead of env

--- middleware/auth.js
+++ middleware/auth.js
@@ -37,7 +37,7 @@ export default async function (context) {
 
   if (!context.$auth.user) {
     return context.redirect(
-      `${process.env.APIURL}/api/v1/auth/init?url=${process.env.WEBSITEURL}${context.route.fullPath}`
+      `${context.env.apiUrl}/api/v1/auth/init?url=${context.env.baseUrl}${context.route.fullPath}`
     )
   }
 }
