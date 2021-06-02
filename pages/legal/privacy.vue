5e06dc0fa874cd1a226ee1176843e67a9a4c1cc8
NightYagi <koruten@gmail.com>
2021-06-01 22:42:42 -0700

    Update legal

--- pages/legal/privacy.vue
+++ pages/legal/privacy.vue
@@ -4,10 +4,10 @@
       <h1>Privacy Policy</h1>
 
       <p>
-        At XIVMods, accessible from {{ siteUrl }}, one of our main priorities is
-        the privacy of our visitors. This Privacy Policy document contains types
-        of information that is collected and recorded by XIVMods and how we use
-        it.
+        At The XIV Repository, accessible from {{ siteUrl }}, one of our main
+        priorities is the privacy of our visitors. This Privacy Policy document
+        contains types of information that is collected and recorded by The XIV
+        Repository and how we use it.
       </p>
 
       <p>
@@ -19,25 +19,26 @@
       <p>We are a Data Controller of your information.</p>
 
       <p>
-        Guavy LLC legal basis for collecting and using the personal information
-        described in this Privacy Policy depends on the Personal Information we
-        collect and the specific context in which we collect the information:
+        The XIV Repository legal basis for collecting and using the personal
+        information described in this Privacy Policy depends on the Personal
+        Information we collect and the specific context in which we collect the
+        information:
       </p>
       <ul>
-        <li>Guavy LLC needs to perform a contract with you</li>
-        <li>You have given Guavy LLC permission to do so</li>
+        <li>The XIV Repository needs to perform a contract with you</li>
+        <li>You have given The XIV Repository permission to do so</li>
         <li>
-          Processing your personal information is in Guavy LLC legitimate
-          interests
+          Processing your personal information is in The XIV Repository
+          legitimate interests
         </li>
-        <li>Guavy LLC needs to comply with the law</li>
+        <li>The XIV Repository needs to comply with the law</li>
       </ul>
 
       <p>
-        Guavy LLC will retain your personal information only for as long as is
-        necessary for the purposes set out in this Privacy Policy. We will
-        retain and use your information to the extent necessary to comply with
-        our legal obligations, resolve disputes, and enforce our policies.
+        The XIV Repository will retain your personal information only for as
+        long as is necessary for the purposes set out in this Privacy Policy. We
+        will retain and use your information to the extent necessary to comply
+        with our legal obligations, resolve disputes, and enforce our policies.
       </p>
 
       <p>
@@ -65,25 +66,27 @@
       <h2>Log Files</h2>
 
       <p>
-        XIVMods follows a standard procedure of using log files. These files log
-        visitors when they visit websites. All hosting companies do this and a
-        part of hosting services' analytics. The information collected by log
-        files include internet protocol (IP) addresses, browser type, Internet
-        Service Provider (ISP), date and time stamp, referring/exit pages, and
-        possibly the number of clicks. These are not linked to any information
-        that is personally identifiable. The purpose of the information is for
-        analyzing trends, administering the site, tracking users' movement on
-        the website, and gathering demographic information.
+        The XIV Repository follows a standard procedure of using log files.
+        These files log visitors when they visit websites. All hosting companies
+        do this and a part of hosting services' analytics. The information
+        collected by log files include internet protocol (IP) addresses, browser
+        type, Internet Service Provider (ISP), date and time stamp,
+        referring/exit pages, and possibly the number of clicks. These are not
+        linked to any information that is personally identifiable. The purpose
+        of the information is for analyzing trends, administering the site,
+        tracking users' movement on the website, and gathering demographic
+        information.
       </p>
 
       <h2>Cookies and Web Beacons</h2>
 
       <p>
-        Like any other website, XIVMods uses 'cookies'. These cookies are used
-        to store information including visitors' preferences, and the pages on
-        the website that the visitor accessed or visited. The information is
-        used to optimize the users' experience by customizing our web page
-        content based on visitors' browser type and/or other information.
+        Like any other website, The XIV Repository uses 'cookies'. These cookies
+        are used to store information including visitors' preferences, and the
+        pages on the website that the visitor accessed or visited. The
+        information is used to optimize the users' experience by customizing our
+        web page content based on visitors' browser type and/or other
+        information.
       </p>
 
       <p>
@@ -93,46 +96,33 @@
         >.
       </p>
 
-      <h2>Google DoubleClick DART Cookie</h2>
-
-      <p>
-        Google is a third-party vendor on our site. It also uses cookies, known
-        as DART cookies, to serve ads to our site visitors based upon their
-        visit to www.website.com and other sites on the internet. However,
-        visitors may choose to decline the use of DART cookies by visiting the
-        Google ad and content network Privacy Policy at the following URL –
-        <a href="https://policies.google.com/technologies/ads"
-          >https://policies.google.com/technologies/ads</a
-        >
-      </p>
-
       <h2>Privacy Policies</h2>
 
       <P
         >You may consult this list to find the Privacy Policy for each of the
-        advertising partners of XIVMods.</P
+        advertising partners of The XIV Repository.</P
       >
 
       <p>
         Third-party ad servers or ad networks uses technologies like cookies,
         JavaScript, or Web Beacons that are used in their respective
-        advertisements and links that appear on XIVMods, which are sent directly
-        to users' browser. They automatically receive your IP address when this
-        occurs. These technologies are used to measure the effectiveness of
-        their advertising campaigns and/or to personalize the advertising
-        content that you see on websites that you visit.
+        advertisements and links that appear on The XIV Repository, which are
+        sent directly to users' browser. They automatically receive your IP
+        address when this occurs. These technologies are used to measure the
+        effectiveness of their advertising campaigns and/or to personalize the
+        advertising content that you see on websites that you visit.
       </p>
 
       <p>
-        Note that XIVMods has no access to or control over these cookies that
-        are used by third-party advertisers.
+        Note that The XIV Repository has no access to or control over these
+        cookies that are used by third-party advertisers.
       </p>
 
       <h2>Third Party Privacy Policies</h2>
 
       <p>
-        XIVMods's Privacy Policy does not apply to other advertisers or
-        websites. Thus, we are advising you to consult the respective Privacy
+        The XIV Repository's Privacy Policy does not apply to other advertisers
+        or websites. Thus, we are advising you to consult the respective Privacy
         Policies of these third-party ad servers for more detailed information.
         It may include their practices and instructions about how to opt-out of
         certain options.
@@ -142,7 +132,10 @@
         You can choose to disable cookies through your individual browser
         options. To know more detailed information about cookie management with
         specific web browsers, it can be found at the browsers' respective
-        websites. What Are Cookies?
+        websites.
+        <a href="https://www.privacypolicies.com/blog/cookies/"
+          >What Are Cookies?</a
+        >
       </p>
 
       <h2>Children's Information</h2>
@@ -154,11 +147,11 @@
       </p>
 
       <p>
-        XIVMods does not knowingly collect any Personal Identifiable Information
-        from children under the age of 13. If you think that your child provided
-        this kind of information on our website, we strongly encourage you to
-        contact us immediately and we will do our best efforts to promptly
-        remove such information from our records.
+        The XIV Repository does not knowingly collect any Personal Identifiable
+        Information from children under the age of 13. If you think that your
+        child provided this kind of information on our website, we strongly
+        encourage you to contact us immediately and we will do our best efforts
+        to promptly remove such information from our records.
       </p>
 
       <h2>Online Privacy Policy Only</h2>
@@ -166,8 +159,9 @@
       <p>
         This Privacy Policy applies only to our online activities and is valid
         for visitors to our website with regards to the information that they
-        shared and/or collect in XIVMods. This policy is not applicable to any
-        information collected offline or via channels other than this website.
+        shared and/or collect in The XIV Repository. This policy is not
+        applicable to any information collected offline or via channels other
+        than this website.
       </p>
 
       <h2>Consent</h2>
@@ -196,13 +190,13 @@ export default {
   },
   auth: false,
   head: {
-    title: 'Privacy - XIVMods',
+    title: 'Privacy - The XIV Repository',
     meta: [
       {
         hid: 'description',
         name: 'description',
         content:
-          'The privacy policy of XIVMods, an open source modding platform. XIVMods currently supports Minecraft, including the forge and fabric mod loaders.',
+          'The privacy policy of The XIV Repository, an open source modding platform. The XIV Repository currently supports Minecraft, including the forge and fabric mod loaders.',
       },
       {
         hid: 'apple-mobile-web-app-title',
--- pages/legal/terms.vue
+++ pages/legal/terms.vue
@@ -6,9 +6,9 @@
       <h2>1. Terms</h2>
 
       <p>
-        By accessing this Website, accessible from ${process.env.baseUrl}, you
-        are agreeing to be bound by these Website Terms and Conditions of Use
-        and agree that you are responsible for the agreement with any applicable
+        By accessing this Website, accessible from {{ siteUrl }}, you are
+        agreeing to be bound by these Website Terms and Conditions of Use and
+        agree that you are responsible for the agreement with any applicable
         local laws. If you disagree with any of these terms, you are prohibited
         from accessing this site. The materials contained in this Website are
         protected by copyright and trade mark law.
@@ -18,9 +18,9 @@
 
       <p>
         Permission is granted to temporarily download one copy of the materials
-        on Guavy LLC's Website for personal, non-commercial transitory viewing
-        only. This is the grant of a license, not a transfer of title, and under
-        this license you may not:
+        on The XIV Repository's Website for personal, non-commercial transitory
+        viewing only. This is the grant of a license, not a transfer of title,
+        and under this license you may not:
       </p>
 
       <ul>
@@ -30,8 +30,8 @@
           display;
         </li>
         <li>
-          attempt to reverse engineer any software contained on Guavy LLC's
-          Website;
+          attempt to reverse engineer any software contained on The XIV
+          Repository's Website;
         </li>
         <li>
           remove any copyright or other proprietary notations from the
@@ -44,8 +44,8 @@
       </ul>
 
       <p>
-        This will let Guavy LLC to terminate upon violations of any of these
-        restrictions. Upon termination, your viewing right will also be
+        This will let The XIV Repository to terminate upon violations of any of
+        these restrictions. Upon termination, your viewing right will also be
         terminated and you should destroy any downloaded materials in your
         possession whether it is printed or electronic format.
       </p>
@@ -53,51 +53,52 @@
       <h2>3. Disclaimer</h2>
 
       <p>
-        All the materials on Guavy LLC’s Website are provided "as is". Guavy LLC
-        makes no warranties, may it be expressed or implied, therefore negates
-        all other warranties. Furthermore, Guavy LLC does not make any
-        representations concerning the accuracy or reliability of the use of the
-        materials on its Website or otherwise relating to such materials or any
-        sites linked to this Website.
+        All the materials on The XIV Repository’s Website are provided "as is".
+        The XIV Repository makes no warranties, may it be expressed or implied,
+        therefore negates all other warranties. Furthermore, The XIV Repository
+        does not make any representations concerning the accuracy or reliability
+        of the use of the materials on its Website or otherwise relating to such
+        materials or any sites linked to this Website.
       </p>
 
       <h2>4. Limitations</h2>
 
       <p>
-        Guavy LLC or its suppliers will not be hold accountable for any damages
-        that will arise with the use or inability to use the materials on Guavy
-        LLC’s Website, even if Guavy LLC or an authorize representative of this
-        Website has been notified, orally or written, of the possibility of such
-        damage. Some jurisdiction does not allow limitations on implied
-        warranties or limitations of liability for incidental damages, these
-        limitations may not apply to you.
+        The XIV Repository or its suppliers will not be hold accountable for any
+        damages that will arise with the use or inability to use the materials
+        on Guavy LLC’s Website, even if The XIV Repository or an authorize
+        representative of this Website has been notified, orally or written, of
+        the possibility of such damage. Some jurisdiction does not allow
+        limitations on implied warranties or limitations of liability for
+        incidental damages, these limitations may not apply to you.
       </p>
 
       <h2>5. Revisions and Errata</h2>
 
       <p>
-        The materials appearing on Guavy LLC’s Website may include technical,
-        typographical, or photographic errors. Guavy LLC will not promise that
-        any of the materials in this Website are accurate, complete, or current.
-        Guavy LLC may change the materials contained on its Website at any time
-        without notice. Guavy LLC does not make any commitment to update the
-        materials.
+        The materials appearing on The XIV Repository’s Website may include
+        technical, typographical, or photographic errors. The XIV Repository
+        will not promise that any of the materials in this Website are accurate,
+        complete, or current. The XIV Repository may change the materials
+        contained on its Website at any time without notice. The XIV Repository
+        does not make any commitment to update the materials.
       </p>
 
       <h2>6. Links</h2>
 
       <p>
-        Guavy LLC has not reviewed all of the sites linked to its Website and is
-        not responsible for the contents of any such linked site. The presence
-        of any link does not imply endorsement by Guavy LLC of the site. The use
-        of any linked website is at the user’s own risk.
+        The XIV Repository has not reviewed all of the sites linked to its
+        Website and is not responsible for the contents of any such linked site.
+        The presence of any link does not imply endorsement by The XIV
+        Repository of the site. The use of any linked website is at the user’s
+        own risk.
       </p>
 
       <h2>7. Site Terms of Use Modifications</h2>
 
       <p>
-        Guavy LLC may revise these Terms of Use for its Website at any time
-        without prior notice. By using this Website, you are agreeing to be
+        The XIV Repository may revise these Terms of Use for its Website at any
+        time without prior notice. By using this Website, you are agreeing to be
         bound by the current version of these Terms and Conditions of Use.
       </p>
 
@@ -111,8 +112,8 @@
       <h2>9. Governing Law</h2>
 
       <p>
-        Any claim related to Guavy LLC's Website shall be governed by the laws
-        of us without regards to its conflict of law provisions.
+        Any claim related to The XIV Repository's Website shall be governed by
+        the laws of us without regards to its conflict of law provisions.
       </p>
 
       <h2>10. Content</h2>
@@ -120,33 +121,34 @@
       <p>
         When you upload text, software, mods, scripts, graphics, photos, audio,
         videos, links, interactive features and other materials that may be
-        viewed on, or accessed through XIVMods, we refer to it as “Content”.
+        viewed on, or accessed through The XIV Repository, we refer to it as
+        “Content”.
       </p>
 
       <ul>
         <li>
           You must own or have the necessary licenses, rights, consents, and
           permissions to store, share or distribute the Content that is uploaded
-          under your XIVMods account.
+          under your The XIV Repository account.
         </li>
         <li>
           You are responsible for all activity and Content that is uploaded
-          under your XIVMods account.
+          under your The XIV Repository account.
         </li>
         <li>
           You must not transmit any viruses, worms, malware, or any other code
-          of a destructive nature through XIVMods.
+          of a destructive nature through The XIV Repository.
         </li>
         <li>
           You retain all of your ownership rights to your Content. We do not
           claim any ownership in or to any of your Content.
         </li>
         <li>
-          To enable us to provide the services of XIVMods, you hereby grant us a
-          worldwide, non-exclusive, royalty-free, and unrestricted license to
-          use, reproduce, distribute copies, prepare derivative works of, or
-          display Content in connection with XIVMods in any medium and for any
-          purpose (including commercial purposes).
+          To enable us to provide the services of The XIV Repository, you hereby
+          grant us a worldwide, non-exclusive, royalty-free, and unrestricted
+          license to use, reproduce, distribute copies, prepare derivative works
+          of, or display Content in connection with The XIV Repository in any
+          medium and for any purpose (including commercial purposes).
         </li>
       </ul>
     </div>
@@ -161,15 +163,21 @@ export default {
   components: {
     MFooter,
   },
+  props: {
+    siteUrl: {
+      type: String,
+      default: process.env.baseUrl,
+    },
+  },
   auth: false,
   head: {
-    title: 'Terms - XIVMods',
+    title: 'Terms - The XIV Repository',
     meta: [
       {
         hid: 'description',
         name: 'description',
         content:
-          'The Terms of Service of XIVMods, an open source modding platform. XIVMods currently supports Minecraft, including the forge and fabric mod loaders.',
+          'The Terms of Service of The XIV Repository, an open source modding platform. The XIV Repository currently supports Minecraft, including the forge and fabric mod loaders.',
       },
       {
         hid: 'apple-mobile-web-app-title',
